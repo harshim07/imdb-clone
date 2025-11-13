6e5b9939804917171a368840bc984593ba7d3ee1
harshitha <harshithamorusu@gmail.com>
2025-11-13 22:33:14 +0530

    add login file

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import UrlValidator from './UrlValidator';
+import LoginForm from './LoginForm';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <UrlValidator />
+    <LoginForm />
   </React.StrictMode>
 );
 
