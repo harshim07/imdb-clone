1b03433424e6429d5986f5f8f53e2c205804ab2c
harshitha <harshithamorusu@gmail.com>
2025-11-13 22:26:42 +0530

    add url file

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import SearchBar from './SearchBar';
+import UrlValidator from './UrlValidator';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <SearchBar />
+    <UrlValidator />
   </React.StrictMode>
 );
 
